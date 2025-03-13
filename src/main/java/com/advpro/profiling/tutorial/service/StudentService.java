928f2820560f460390d682660df4428d4263a9fc
alicfamahdi <alicfamahdi@gmail.com>
2025-03-14 01:54:36 +0700

    feat: [Refactoring] - Optimizing method highestGpa and joinStudentName

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -28,25 +28,21 @@ public class StudentService {
     }
 
     public Optional<Student> findStudentWithHighestGpa() {
-        List<Student> students = studentRepository.findAll();
-        Student highestGpaStudent = null;
-        double highestGpa = 0.0;
-        for (Student student : students) {
-            if (student.getGpa() > highestGpa) {
-                highestGpa = student.getGpa();
-                highestGpaStudent = student;
-            }
-        }
-        return Optional.ofNullable(highestGpaStudent);
+        return studentRepository.findStudentWithHighestGpa();
     }
 
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
+//        String result = "";
+//        for (Student student : students) {
+//            result += student.getName() + ", ";
+//        }
+//        return result.substring(0, result.length() - 2);
+        StringBuilder result = new StringBuilder();
         for (Student student : students) {
-            result += student.getName() + ", ";
+            result.append(student.getName()).append(", ");
         }
-        return result.substring(0, result.length() - 2);
+        return result.length() > 0 ? result.substring(0, result.length() - 2) : "";
     }
 }
 
